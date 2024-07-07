6c6e9046e095a88f08f606afb7ac84856dd14f61
gopikrishnaA <gopi.08403@gmail.com>
2024-07-07 14:31:28 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -51,3 +51,32 @@ INNER JOIN
     temp_wcis_legalentity wcis
     ON cust.wcis_id <> wcis.clientid
     AND cust.clean_short_name = wcis.clean_legal_trade_name;
+------------------------------------------------------------------
+
+
+-- Create temporary tables with preprocessed names
+CREATE TEMP TABLE temp_liq_customer AS
+SELECT
+    wcis_id,
+    upper(regexp_replace(short_name, '\\W', '')) AS clean_short_name
+FROM
+    v_liq_customer
+WHERE
+    wcis_id = '230527317';
+
+CREATE TEMP TABLE temp_wcis_legalentity AS
+SELECT
+    clientid,
+    upper(regexp_replace(legal_trade_name, '\\W', '')) AS clean_legal_trade_name
+FROM
+    v_wcis_legalentity;
+
+-- Optimized query using temporary tables
+SELECT
+    COUNT(*) AS duplicates
+FROM
+    temp_liq_customer cust
+INNER JOIN
+    temp_wcis_legalentity wcis
+    ON cust.wcis_id <> wcis.clientid
+    AND cust.clean_short_name = wcis.clean_legal_trade_name;
